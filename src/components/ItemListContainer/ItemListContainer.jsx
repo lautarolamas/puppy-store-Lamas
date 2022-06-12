9180f07f97e04fc19e74e7857e3911188cdd6bbb
lautarolamas <lautilamas7@gmail.com>
2022-06-12 17:16:31 -0300

    agregandole prop greting al componente ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const ItemListContainer = () => {
+export const ItemListContainer = ({ greeting }) => {
   return (
     <div>
-      <div> $7500 ALIMENTO PRO PLAN 20KG</div>
-      <div> $2300 ALIMENTO PRO PLAN 22KG</div>
-      <div>$2500 ARNES PARA PERROS</div>
-      <div>$6000 PURINA CAT CHOW </div>
-      <div>$6000 PURINA CAT CHOW </div>
+      <div> $7500 ALIMENTO PRO PLAN 20KG {greeting}</div>
+      <div> $2300 ALIMENTO PRO PLAN 22KG {greeting}</div>
+      <div>$2500 ARNES PARA PERROS {greeting}</div>
+      <div>$6000 PURINA CAT CHOW {greeting} </div>
+      <div>$6000 PURINA CAT CHOW {greeting} </div>
     </div>
   );
 };
