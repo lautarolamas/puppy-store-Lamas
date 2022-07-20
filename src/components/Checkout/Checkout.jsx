5f0200fdcaa75abc95efbfec26e9c819ad6d990d
lautarolamas <lautilamas7@gmail.com>
2022-07-20 00:23:45 -0300

    agregando input phone

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -29,7 +29,7 @@ import { BsGithub, BsDiscord, BsPerson } from "react-icons/bs";
 
 export default function Checkout() {
   return (
-    <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
+    <Container bg="#ffff" maxW="full" mt={0} centerContent overflow="hidden">
       <Flex>
         <Box
           bg="#02054B"
@@ -42,78 +42,21 @@ export default function Checkout() {
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
                 <Box>
-                  <Heading>Contact</Heading>
+                  <Heading>Resumen de la compra</Heading>
                   <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
                     A continuación se encuentran los productos que vas a comprar
                   </Text>
                   <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}
-                      >
-                        +91-988888888
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}
-                      >
-                        hello@abc.com
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
-                      >
-                        Karnavati, India
-                      </Button>
-                    </VStack>
+                    <VStack pl={0} spacing={3} alignItems="flex-start"></VStack>
                   </Box>
                   <HStack
                     mt={{ lg: 10, md: 10 }}
                     spacing={5}
                     px={5}
                     alignItems="flex-start"
-                  >
-                    <IconButton
-                      aria-label="facebook"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: "#0D74FF" }}
-                      icon={<MdFacebook size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: "#0D74FF" }}
-                      icon={<BsGithub size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="discord"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: "#0D74FF" }}
-                      icon={<BsDiscord size="28px" />}
-                    />
-                  </HStack>
+
+                    // ACA VAN A IR LOS PRODUCTOS
+                  ></HStack>
                 </Box>
               </WrapItem>
               <WrapItem>
@@ -130,14 +73,24 @@ export default function Checkout() {
                           <Input type="text" size="md" />
                         </InputGroup>
                       </FormControl>
-                      <FormControl id="name">
+                      <FormControl id="celular">
+                        <FormLabel>Telefono</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<MdPhone color="gray.800" />}
+                          />
+                          <Input type="number" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="mail">
                         <FormLabel>Mail</FormLabel>
                         <InputGroup borderColor="#E0E1E7">
                           <InputLeftElement
                             pointerEvents="none"
                             children={<MdOutlineEmail color="gray.800" />}
                           />
-                          <Input type="text" size="md" />
+                          <Input type="email" size="md" />
                         </InputGroup>
                       </FormControl>
                       <FormControl id="name" float="right">
